736123cd68e03cf727d85789a57dd9c328e0698c
Morgan Skillicorn <hello@morgan.games>
2025-02-10 02:14:37 +0000

    functional trainer

--- trainmodel.py
+++ trainmodel.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 from torch import inference_mode
 from torch.utils.data import DataLoader
+import torchvision
 from torchvision import datasets, transforms
 from torchinfo import summary
 import random
@@ -14,6 +15,8 @@ import matplotlib.pyplot as pyplot
 import seaborn
 import time
 import torchinfo
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
 
 if __name__ == "__main__":    
 
@@ -30,13 +33,13 @@ if __name__ == "__main__":
     TRAIN_DIRECTORY = "Dataset/train/train/all"
     TEST_DIRECTORY = "Dataset/test_origin/test_origin"
 
-    IMAGE_WIDTH = 256
-    IMAGE_HEIGHT = 256
+    IMAGE_WIDTH = 512
+    IMAGE_HEIGHT = 512
     IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
 
     SEED = 70
-    NUM_EPOCHS = 30
-    BATCH_SIZE = 10
+    NUM_EPOCHS = 20
+    BATCH_SIZE = 20
 
     random.seed(SEED)
     seaborn.set_theme()
@@ -305,14 +308,16 @@ if __name__ == "__main__":
                 optimizer: torch.optim.Optimizer):
         # Put model in train mode
         model.train()
-        print("\nTraining model...")        
+        print("\nTraining model...")
         
+        # Start the timer
+        start_time = timer()
+
         # Setup train loss and train accuracy values
         train_loss, train_acc = 0, 0
         
         # Loop through data loader data batches
-        for batch, (X, y) in enumerate(dataloader):
-            print(f"Training on tensor: {batch}")
+        for batch, (X, y) in tqdm(enumerate(dataloader)):
 
             # Send data to target device
             X, y = X.to(DEVICE), y.to(DEVICE)
@@ -335,11 +340,15 @@ if __name__ == "__main__":
 
             # Calculate and accumulate accuracy metric across all batches
             y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-            train_acc += (y_pred_class == y).sum().item()/len(y_pred)           
+            train_acc += (y_pred_class == y).sum().item()/len(y_pred)      
 
         # Adjust metrics to get average loss and accuracy per batch 
         train_loss = train_loss / len(dataloader)
         train_acc = train_acc / len(dataloader)
+
+        end_time = timer()
+        print(f"Batch training time: {end_time-start_time:.3f} seconds")
+
         return train_loss, train_acc
 
     def test_step(model: torch.nn.Module, 
@@ -349,14 +358,16 @@ if __name__ == "__main__":
         model.eval() 
         print("\nEvaluating model...")  
         
+        # Start the timer
+        start_time = timer()
+        
         # Setup test loss and test accuracy values
         test_loss, test_acc = 0, 0
         
         # Turn on inference context manager
         with torch.inference_mode():
             # Loop through DataLoader batches
-            for batch, (X, y) in enumerate(dataloader):
-                print(f"Evaluating on tensor: {batch}")
+            for batch, (X, y) in tqdm(enumerate(dataloader)):
                 # Send data to target device
                 X, y = X.to(DEVICE), y.to(DEVICE)
         
@@ -374,6 +385,10 @@ if __name__ == "__main__":
         # Adjust metrics to get average loss and accuracy per batch 
         test_loss = test_loss / len(dataloader)
         test_acc = test_acc / len(dataloader)
+
+        end_time = timer()
+        print(f"Batch evaluating time: {end_time-start_time:.3f} seconds")
+
         return test_loss, test_acc
 
     # 1. Take in various parameters required for training and test steps
@@ -392,8 +407,8 @@ if __name__ == "__main__":
         }
         
         # 3. Loop through training and testing steps for a number of epochs
-        for epoch in range(epochs):
-            print(f"\nTraining epoch: {epoch}")
+        for epoch in tqdm(range(epochs)):
+            print(f"\n\n\nTraining epoch: {epoch}")
             train_loss, train_acc = train_step(model=model,
                                             dataloader=train_dataloader,
                                             loss_fn=loss_fn,
@@ -405,10 +420,10 @@ if __name__ == "__main__":
             # 4. Print out what's happening
             print(
                 f"Epoch: {epoch+1} | "
-                f"train_loss: {train_loss:.4f} | "
-                f"train_acc: {train_acc:.4f} | "
-                f"test_loss: {test_loss:.4f} | "
-                f"test_acc: {test_acc:.4f}"
+                f"training loss: {train_loss:.4f} | "
+                f"training accuracy: {train_acc:.4f} | "
+                f"testing loss: {test_loss:.4f} | "
+                f"test accuracy: {test_acc:.4f}"
             )
 
             # 5. Update results dictionary
@@ -428,6 +443,9 @@ if __name__ == "__main__":
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 
+    # Start the timer
+    start_time = timer()
+
     # Train model_0 
     model_results = train(model=model,
                         train_dataloader=train_dataloader_augmented,
@@ -435,6 +453,9 @@ if __name__ == "__main__":
                         optimizer=optimizer,
                         loss_fn=loss_fn,
                         epochs=NUM_EPOCHS)
+    
+    end_time = timer()
+    print(f"Total training time: {end_time-start_time:.3f} seconds")
 
     folder = "models"
     base_filename = "moire_model.pth"
@@ -502,3 +523,170 @@ if __name__ == "__main__":
         print(f"Plot saved to: {save_path}")
 
     plot_loss_curves(model_results, counter)
+    
+    # Choose a image.
+    custom_image_path = "Dataset/test_origin/test_origin/moire/0000_moire.jpg"
+
+    # Load in custom image and convert the tensor values to float32
+    custom_image = torchvision.io.read_image(str(custom_image_path)).type(torch.float32)
+
+    # Divide the image pixel values by 255 to get them between [0, 1]
+    custom_image = custom_image / 255. 
+
+    # Print out image data
+    print(f"Custom image tensor:\n{custom_image}\n")
+    print(f"Custom image shape: {custom_image.shape}\n")
+    print(f"Custom image dtype: {custom_image.dtype}")
+
+    custom_image_transform = transforms.Compose([
+        transforms.Resize(IMAGE_SIZE),
+    ])
+
+    # Transform target image
+    custom_image_transformed = custom_image_transform(custom_image)
+
+    # Print out original shape and new shape
+    print(f"Original shape: {custom_image.shape}")
+    print(f"New shape: {custom_image_transformed.shape}")
+
+    model.eval()
+    with torch.inference_mode():
+        # Add an extra dimension to image
+        custom_image_transformed_with_batch_size = custom_image_transformed.unsqueeze(dim=0)
+        
+        # Print out different shapes
+        print(f"Custom image transformed shape: {custom_image_transformed.shape}")
+        print(f"Unsqueezed custom image shape: {custom_image_transformed_with_batch_size.shape}")
+        
+        # Make a prediction on image with an extra dimension
+        custom_image_pred = model(custom_image_transformed.unsqueeze(dim=0).to(DEVICE))
+
+        # Let's convert them from logits -> prediction probabilities -> prediction labels
+    # Print out prediction logits
+    print(f"Prediction logits: {custom_image_pred}")
+
+    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    custom_image_pred_probs = torch.softmax(custom_image_pred, dim=1)
+    print(f"Prediction probabilities: {custom_image_pred_probs}")
+
+    # Convert prediction probabilities -> prediction labels
+    custom_image_pred_label = torch.argmax(custom_image_pred_probs, dim=1)
+    print(f"Prediction label: {custom_image_pred_label}")
+
+    # put pred label to CPU, otherwise will error
+    custom_image_pred_class = class_names[custom_image_pred_label.cpu()]
+
+    # Plot custom image
+    pyplot.imshow(custom_image.permute(1, 2, 0)) # need to permute image dimensions from CHW -> HWC otherwise matplotlib will error
+    pyplot.title(f"Image shape: {custom_image.shape} Class: {custom_image_pred_class}")
+    pyplot.axis(False)
+
+    directory = "Dataset/test_origin/test_origin/clean"
+
+    moire = 0
+    clean = 0
+
+    # Iterate over each file in the directory
+    for filename in os.listdir(directory):
+        # Construct full file path
+        file_path = os.path.join(directory, filename)
+        
+        # Check if it is a file (not a directory)
+        if os.path.isfile(file_path):
+            print(f"Found file: {filename}")
+
+        custom_image_path = file_path
+
+        # Load in custom image and convert the tensor values to float32
+        custom_image = torchvision.io.read_image(str(custom_image_path)).type(torch.float32)
+
+        # Divide the image pixel values by 255 to get them between [0, 1]
+        custom_image = custom_image / 255. 
+
+        custom_image_transform = transforms.Compose([
+            transforms.Resize(IMAGE_SIZE),
+        ])
+
+        # Transform target image
+        custom_image_transformed = custom_image_transform(custom_image)
+
+        model.eval()
+        with torch.inference_mode():
+            # Add an extra dimension to image
+            custom_image_transformed_with_batch_size = custom_image_transformed.unsqueeze(dim=0)
+            
+            # Make a prediction on image with an extra dimension
+            custom_image_pred = model(custom_image_transformed.unsqueeze(dim=0).to(DEVICE))
+
+            # Let's convert them from logits -> prediction probabilities -> prediction labels
+
+        # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+        custom_image_pred_probs = torch.softmax(custom_image_pred, dim=1)
+
+        # Convert prediction probabilities -> prediction labels
+        custom_image_pred_label = torch.argmax(custom_image_pred_probs, dim=1)
+
+        custom_image_pred_class = class_names[custom_image_pred_label.cpu()] # put pred label to CPU, otherwise will error
+        custom_image_pred_class
+
+        if custom_image_pred_class == "moire":
+            moire += 1
+        else:
+            clean += 1
+
+        print(f"moire: {moire} clean: {clean}")
+
+        directory = "Dataset/test_origin/test_origin/moire"
+
+    moire = 0
+    clean = 0
+
+    # Iterate over each file in the directory
+    for filename in os.listdir(directory):
+        # Construct full file path
+        file_path = os.path.join(directory, filename)
+        
+        # Check if it is a file (not a directory)
+        if os.path.isfile(file_path):
+            print(f"Found file: {filename}")
+
+        custom_image_path = file_path
+
+        # Load in custom image and convert the tensor values to float32
+        custom_image = torchvision.io.read_image(str(custom_image_path)).type(torch.float32)
+
+        # Divide the image pixel values by 255 to get them between [0, 1]
+        custom_image = custom_image / 255. 
+
+        custom_image_transform = transforms.Compose([
+            transforms.Resize(IMAGE_SIZE),
+        ])
+
+        # Transform target image
+        custom_image_transformed = custom_image_transform(custom_image)
+
+        model.eval()
+        with torch.inference_mode():
+            # Add an extra dimension to image
+            custom_image_transformed_with_batch_size = custom_image_transformed.unsqueeze(dim=0)
+            
+            # Make a prediction on image with an extra dimension
+            custom_image_pred = model(custom_image_transformed.unsqueeze(dim=0).to(DEVICE))
+
+            # Let's convert them from logits -> prediction probabilities -> prediction labels
+
+        # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+        custom_image_pred_probs = torch.softmax(custom_image_pred, dim=1)
+
+        # Convert prediction probabilities -> prediction labels
+        custom_image_pred_label = torch.argmax(custom_image_pred_probs, dim=1)
+
+        custom_image_pred_class = class_names[custom_image_pred_label.cpu()] # put pred label to CPU, otherwise will error
+        custom_image_pred_class
+
+        if custom_image_pred_class == "moire":
+            moire += 1
+        else:
+            clean += 1
+
+        print(f"moire: {moire} clean: {clean}")
